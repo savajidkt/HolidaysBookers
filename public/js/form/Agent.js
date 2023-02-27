f2280cd9f6ff12663d0a2c8daa3338cb317c4757
JJParejiya <jj.parejiya@gmail.com>
2023-02-27 17:03:55 +0530

    add form validation

--- public/js/form/Agent.js
+++ public/js/form/Agent.js
@@ -22,19 +22,20 @@ var FrmAgentPreference = function () {
                 agent_state: { required: true },
                 agent_city: { required: true },
                 agent_pincode: { required: true },
-                agent_mobile_number: { required: true },
+                agent_mobile_number: { required: true, digits:true },
+                agent_telephone: { digits:true },
                 agent_email: { required: true, email: true },
                 mgmt_first_name: { required: true },
                 mgmt_last_name: { required: true },
-                mgmt_contact_number: { required: true },
+                mgmt_contact_number: { required: true, digits:true },
                 mgmt_email: { required: true, email: true },
                 account_first_name: { required: true },
                 account_last_name: { required: true },
-                account_contact_number: { required: true },
+                account_contact_number: { required: true, digits:true },
                 account_email: { required: true, email: true },
                 reserve_first_name: { required: true },
                 reserve_last_name: { required: true },
-                reserve_contact_number: { required: true },
+                reserve_contact_number: { required: true, digits:true },
                 reserve_email: { required: true, email: true },
                 agent_username: { required: true },
                 agent_password: { minlength: 6, required: true },
@@ -43,95 +44,95 @@ var FrmAgentPreference = function () {
             },
             messages: {
                 agent_company_name: {
-                    required: $("input[name=agent_company_name]").attr('data-error')+' is required.'
+                    required: $("input[name=agent_company_name]").attr('data-error')+' is required'
                 },
                 agent_company_type: {
-                    required: $("input[name=agent_company_type]").attr('data-error')
+                    required: $("input[name=agent_company_type]").attr('data-error')+' is required'
                 },
                 nature_of_business: {
-                    required: $("input[name=nature_of_business]").attr('data-error')
+                    required: $("input[name=nature_of_business]").attr('data-error')+' is required'
                 },
                 agent_first_name: {
-                    required: $("input[name=agent_first_name]").attr('data-error')
+                    required: $("input[name=agent_first_name]").attr('data-error')+' is required'
                 },
                 agent_last_name: {
-                    required: $("input[name=agent_last_name]").attr('data-error')
+                    required: $("input[name=agent_last_name]").attr('data-error')+' is required'
                 },
                 agent_designation: {
-                    required: $("input[name=agent_designation]").attr('data-error')
+                    required: $("input[name=agent_designation]").attr('data-error')+' is required'
                 },
                 agent_dob: {
-                    required: $("input[name=agent_dob]").attr('data-error')
+                    required: $("input[name=agent_dob]").attr('data-error')+' is required'
                 },
                 agent_office_address: {
-                    required: $("input[name=agent_office_address]").attr('data-error')
+                    required: $("input[name=agent_office_address]").attr('data-error')+' is required'
                 },
                 agent_country: {
-                    required: $("input[name=agent_country]").attr('data-error')
+                    required: $("input[name=agent_country]").attr('data-error')+' is required'
                 },
                 agent_state: {
-                    required: $("input[name=agent_state]").attr('data-error')
+                    required: $("input[name=agent_state]").attr('data-error')+' is required'
                 },
                 agent_city: {
-                    required: $("input[name=agent_city]").attr('data-error')
+                    required: $("input[name=agent_city]").attr('data-error')+' is required'
                 },
                 agent_pincode: {
-                    required: $("input[name=agent_pincode]").attr('data-error')
+                    required: $("input[name=agent_pincode]").attr('data-error')+' is required'
                 },
                 agent_mobile_number: {
-                    required: $("input[name=agent_mobile_number]").attr('data-error')
+                    required: $("input[name=agent_mobile_number]").attr('data-error')+' is required'
                 },
                 agent_email: {
-                    required: $("input[name=agent_email]").attr('data-error')
+                    required: $("input[name=agent_email]").attr('data-error')+' is required'
                 },
                 mgmt_first_name: {
-                    required: $("input[name=mgmt_first_name]").attr('data-error')
+                    required: $("input[name=mgmt_first_name]").attr('data-error')+' is required'
                 },
                 mgmt_last_name: {
-                    required: $("input[name=mgmt_last_name]").attr('data-error')
+                    required: $("input[name=mgmt_last_name]").attr('data-error')+' is required'
                 },
                 mgmt_contact_number: {
-                    required: $("input[name=mgmt_contact_number]").attr('data-error')
+                    required: $("input[name=mgmt_contact_number]").attr('data-error')+' is required'
                 },
                 mgmt_email: {
-                    required: $("input[name=mgmt_email]").attr('data-error')
+                    required: $("input[name=mgmt_email]").attr('data-error')+' is required'
                 },
                 account_first_name: {
-                    required: $("input[name=account_first_name]").attr('data-error')
+                    required: $("input[name=account_first_name]").attr('data-error')+' is required'
                 },
                 account_last_name: {
-                    required: $("input[name=account_last_name]").attr('data-error')
+                    required: $("input[name=account_last_name]").attr('data-error')+' is required'
                 },
                 account_contact_number: {
-                    required: $("input[name=account_contact_number]").attr('data-error')
+                    required: $("input[name=account_contact_number]").attr('data-error')+' is required'
                 },
                 account_email: {
-                    required: $("input[name=account_email]").attr('data-error')
+                    required: $("input[name=account_email]").attr('data-error')+' is required'
                 },
                 reserve_first_name: {
-                    required: $("input[name=reserve_first_name]").attr('data-error')
+                    required: $("input[name=reserve_first_name]").attr('data-error')+' is required'
                 },
                 reserve_last_name: {
-                    required: $("input[name=reserve_last_name]").attr('data-error')
+                    required: $("input[name=reserve_last_name]").attr('data-error')+' is required'
                 },
                 reserve_contact_number: {
-                    required: $("input[name=reserve_contact_number]").attr('data-error')
+                    required: $("input[name=reserve_contact_number]").attr('data-error')+' is required'
                 },
                 reserve_email: {
-                    required: $("input[name=reserve_email]").attr('data-error')
+                    required: $("input[name=reserve_email]").attr('data-error')+' is required'
                 },
                 agent_username: {
-                    required: $("input[name=agent_username]").attr('data-error')
+                    required: $("input[name=agent_username]").attr('data-error')+' is required'
                 },
                 agent_password: {
-                    required: $("input[name=agent_password]").attr('data-error')
+                    required: $("input[name=agent_password]").attr('data-error')+' is required'
                 },
                 agent_confirm_password: {
-                    required: $("input[name=agent_confirm_password]").attr('data-error')
+                    required: $("input[name=agent_confirm_password]").attr('data-error')+' is required'
                 },
 
                 status: {
-                    required: $("select[name=status]").attr('data-error')
+                    required: $("select[name=status]").attr('data-error')+' is required'
                 },
             },
             errorPlacement: function (error, element) {
