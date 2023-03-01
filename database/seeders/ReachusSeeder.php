45506eb1e818ff0be75fed681e746c6d3c71ffcf
JJParejiya <jj.parejiya@gmail.com>
2023-03-01 15:15:22 +0530

    jQuery Fix hide show option

--- database/seeders/ReachusSeeder.php
+++ database/seeders/ReachusSeeder.php
@@ -16,30 +16,44 @@ class ReachusSeeder extends Seeder
     {
         Reach::create([
             'name' => 'Email Marketing',
+            'show_other_textbox' => 0,
+            'textbox_lable' => '',
             'status' => 1
         ]);
         Reach::create([
             'name' => 'Trade Show',
+            'show_other_textbox' => 0,
+            'textbox_lable' => '',
             'status' => 1
         ]);
         Reach::create([
             'name' => 'Search Engine',
+            'show_other_textbox' => 0,
+            'textbox_lable' => '',
             'status' => 1
         ]);
         Reach::create([
             'name' => 'Advertisement',
+            'show_other_textbox' => 0,
+            'textbox_lable' => '',
             'status' => 1
         ]);
         Reach::create([
             'name' => 'Bussiness Associate referral',
+            'show_other_textbox' => 0,
+            'textbox_lable' => '',
             'status' => 1
         ]);
         Reach::create([
             'name' => 'Sales Person',
+            'show_other_textbox' => 1,
+            'textbox_lable' => 'Sales Person Name',
             'status' => 1
         ]);
         Reach::create([
             'name' => 'Other',
+            'show_other_textbox' => 1,
+            'textbox_lable' => 'Specify Other',
             'status' => 1
         ]);
     }
