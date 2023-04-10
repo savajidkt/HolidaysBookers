a3b432f85e0fea720d3e04326b84620bb72688a5
savajidkt <savaji.dkt@gmail.com>
2023-04-10 16:19:28 +0530

    Hotel search lisitng

--- app/Repositories/HotelListingRepository.php
+++ app/Repositories/HotelListingRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repositories;
 
 use App\Models\OfflineHotel;
+use App\Models\OfflineRoom;
+use App\Models\OfflineRoomPrice;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Support\Facades\DB;
@@ -38,8 +40,40 @@ class HotelListingRepository
                 }
             })
             ->paginate(10);
-        
-        return $hotels;
+        $hotelsListingArray = [];
+        foreach($hotels as $key=>$hotel){
+            $hotelsListingArray[$key]['id'] = $hotel->id;
+            $hotelsListingArray[$key]['hotel_country'] = $hotel->country->name;
+            $hotelsListingArray[$key]['hotel_city'] = $hotel->city->name;
+            $hotelsListingArray[$key]['hotel_group_id'] = $hotel->hotelgroup->name;
+            $hotelsListingArray[$key]['property_type_id'] = $hotel->property->property_name;
+            $hotelsListingArray[$key]['api_id'] = $hotel->api_id;
+            $hotelsListingArray[$key]['hotel_code'] = $hotel->hotel_code;
+            $hotelsListingArray[$key]['hotel_name'] = $hotel->hotel_name;
+            $hotelsListingArray[$key]['category'] = $hotel->category;
+            $hotelsListingArray[$key]['phone_number'] = $hotel->phone_number;
+            $hotelsListingArray[$key]['fax_number'] = $hotel->fax_number;
+            $hotelsListingArray[$key]['hotel_address'] = $hotel->hotel_address;
+            $hotelsListingArray[$key]['hotel_pincode'] = $hotel->hotel_pincode;
+            $hotelsListingArray[$key]['currency'] = $hotel->currency;
+            $hotelsListingArray[$key]['hotel_image_location'] = $hotel->hotel_image_location;
+            $hotelsListingArray[$key]['hotel_description'] = $hotel->hotel_description;
+            $hotelsListingArray[$key]['hotel_review'] = $hotel->hotel_review;
+            $hotelsListingArray[$key]['hotel_email'] = $hotel->hotel_email;
+            $hotelsListingArray[$key]['hotel_latitude'] = $hotel->hotel_latitude;
+            $hotelsListingArray[$key]['hotel_longitude'] = $hotel->hotel_longitude;
+            $hotelsListingArray[$key]['is_new'] = $hotel->is_new;
+            $hotelsListingArray[$key]['cancel_days'] = $hotel->cancel_days;
+            $hotelsListingArray[$key]['cancellation_policy'] = $hotel->cancellation_policy;
+            $hotelsListingArray[$key]['hotel_type'] = $hotel->hotel_type;
+            $hotelRoomArray =[];
+            $roomsIds = $hotel->rooms->pluck('id')->toArray();
+
+            $roomPrice = OfflineRoomPrice::whereIn('room_id',$roomsIds)->get();
+            dd($roomPrice->orderBy('price_p_n_single_adult','asc')->limit(1));
+            $hotelsListingArray[$key]['room'] = $roomPrice;
+        }
+        return $hotelsListingArray;
     }
 
     public function hotelCount($request)
